02077f8d5968c7ea19c0546be9f164b7ec8c0535
Andrew <ahlopez1999@gmail.com>
2023-04-03 22:26:56 -0400

    Water Spread Progress

--- scripts/updateWater.gd
+++ scripts/updateWater.gd
@@ -4,18 +4,37 @@ extends Node
 var waterDir = 1
 
 func update_water_spread():
-	# if tile is an ocean tile raise its water height if there are higher water tiles adjacent
-	# else if tile is not already flooded and adjacent tile has water that can spread (water height > 1) then spread water here
-	return
+	var mapHeight = Global.mapHeight
+	var mapWidth = Global.mapWidth
+
+	for i in mapHeight:
+		for j in mapWidth:
+			var currTile = Global.tileMap[i][j]
+			var highest = find_highest_water(currTile)
+			# if there are no higher water tiles around, check if water needs to disapate
+			if highest == 0:
+				pass
+			# if tile is an ocean tile raise its water height if there are higher water tiles adjacent
+			elif currTile.base == Tile.TileBase.OCEAN:
+				return
+			else:
+				return
 
 func find_highest_water(tile):
-	var highest = Tile
+	var highest = tile
 	# Only checking for 4 cardinal directions, can be changed if needed
 	var neighbors = [[tile.i-1, tile.j], [tile.i+1, tile.j], [tile.i, tile.j-1], [tile.i, tile.j+1]]
 	for n in neighbors:
-			if UpdateValue.is_valid_tile(n[0], n[1]):
-				return
-	return
+		if UpdateValue.is_valid_tile(n[0], n[1]):
+			var nwaterheight = Global.tileMap[n[0]][n[1]].waterheight
+			var nbaseheight = Global.tileMap[n[0]][n[1]].baseheight
+			# If neighbor is taller and has water that can spread set it to highest
+			if nbaseheight + nwaterheight > highest.baseheight + highest.waterheight && nwaterheight > 1:
+				highest = Global.tileMap[n[0]][n[1]]
+	if highest == tile:
+		return 0
+	else:
+		return highest.waterheight + highest.baseheight
 	
 #Update waves
 func update_waves():
