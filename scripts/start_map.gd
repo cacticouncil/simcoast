1d378081334c472407a653f1179372be4a981250
Andrew <ahlopez1999@gmail.com>
2023-04-04 23:49:21 -0400

    Merge with Main
    Water spread completed and merged with main resolving merge conflicts. Temporarily disabled # of buildings in the HUD display as it was getting too cluttered and running offscreen.

--- scripts/start_map.gd
+++ scripts/start_map.gd
@@ -337,7 +337,9 @@ func _process(delta):
 
 func update_game_state():
 	#print("Updating game state on tick: " + str(numTicks))
-	UpdateWaves.update_waves()
+	#turning this function off until it can be fixed
+	#UpdateWater.update_waves()
+	UpdateWater.update_water_spread()
 	City.calculate_damage()
 	UpdateValue.update_land_value()
 	UpdateHappiness.update_happiness()
