5af8395e0a49c3da8178929eb1a74e4009fd2bd3
Edward Menello <edmenello@gmail.com>
2023-10-08 20:38:12 -0400

    Changed up NPC observer to trigger when a tile is placed
    for testing purposes only!

--- scripts/Observers/NpcObserver.gd
+++ scripts/Observers/NpcObserver.gd
@@ -1,13 +1,13 @@
 extends "res://scripts/Observers/Observer.gd"
-#Handles SFX
+#Handles NPC triggers
 
 #export var sfxPlayer : AudioStreamPlayer
 
 func onNotify(event, stats):
 	# Plays some banger royalty free construction sfx
-	if event.eventName == "Completed Mission":
-		if event.eventValue == 0:
-			print("Mission 0 Dialogue Start")
+	if event.eventName == "Added Tile":
+		if event.eventValue == 1:
+			print("Added Tile Dialogue Start")
 			NPCOrganizer.npcDictionary[1].dialogueTrigger()
 
 func triggerDialogue(dialogue):
