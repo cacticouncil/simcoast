ad4f91b8e58ad60250b6c53b9bcba03b368bf3d6
Logan Chenicek <ltcollege401@gmail.com>
2023-10-12 22:41:02 -0400

    Changed the shader for the x icon on the menu
    X icon has it's own shader

--- scripts/AchievementMenu.gd
+++ scripts/AchievementMenu.gd
@@ -48,4 +48,17 @@ func _ready():
 
 
 func _on_QuitButton_pressed():
+	$QuitButton.material.set_shader_param("value", 1)
 	get_parent().remove_child(self)
+
+
+func _on_QuitButton_mouse_entered():
+	$QuitButton.material.set_shader_param("value", 0.3)
+
+
+func _on_QuitButton_mouse_exited():
+	$QuitButton.material.set_shader_param("value", 1)
+
+
+func _on_QuitButton_button_down():
+	$QuitButton.material.set_shader_param("value", 0.1)
