bb44e590e86d4c93ef2c39db5fca9e605a53b69d
Logan Chenicek <lchenicek@ufl.edu>
2023-12-04 09:46:30 -0500

    Added a few comments

--- scripts/Observers/BadgeObserver.gd
+++ scripts/Observers/BadgeObserver.gd
@@ -4,12 +4,12 @@ extends "res://scripts/Observers/Observer.gd"
 var badgeScript = preload("res://scripts/Observers/Badge.gd")
 var badges = []
 
+#TODO: Add achievements for badges
 var popupImagePath = "res://assets/achievement_icons/BadgeAchievement.png"
 
 func onNotify(event):
 	if event.eventName == "Unlocked Badge":
 		badges.append(badgeScript.new(event.eventDescription, event.eventDescription + " Description", event.eventValue, "res://assets/badge_icons/test_badge.png"))
-			#get_node("/root/Overlay").achievement_pop(event.eventDescription, popupImagePath)
 
 func getBadges():
 	return badges
