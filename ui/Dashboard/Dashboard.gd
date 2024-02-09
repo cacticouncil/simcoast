faf8b2e619ed3414165f5f534056b7e278c42717
Alexandra Seelig <108905816+lolxela@users.noreply.github.com>
2024-02-08 19:10:55 -0500

    Changed buttons for popups
    Changed button visual for popups. Buttons are plus signs when popup is closed for expand and minus sign when popup is open for minimize

--- ui/Dashboard/Dashboard.gd
+++ ui/Dashboard/Dashboard.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#Displays money and population in City Stats Bar
 	$ColorRect3/CityStatsBar/Money.text = "$" + Econ.comma_values(str(Econ.money))
 	$ColorRect3/CityStatsBar/Population.text = str(UpdatePopulation.get_population())
 
+	#When dashboard is opened, propery, sales, and income taxes are set based on values in Econ.gd
 	$ColorRect3/CityStatsBar/HBoxContainer7/PropertyTaxValue.text = str(Econ.PROPERTY_TAX * 100) + "%"
 	$ColorRect3/CityStatsBar/HBoxContainer7/PropertyTaxSlider.value = Econ.PROPERTY_TAX
 	
@@ -15,34 +16,34 @@ func _ready():
 	$ColorRect3/CityStatsBar/HBoxContainer9/IncomeTaxValue.text = str(Econ.INCOME_TAX * 100) + "%"
 	$ColorRect3/CityStatsBar/HBoxContainer9/IncomeTaxSlider.value = Econ.INCOME_TAX
 
+
+#quit button functions
 func _on_QuitButton_pressed():
 	get_node("/root/CityMap/HUD/TopBarBG/DashboardSelected").visible = false
 	$QuitButton.material.set_shader_param("value", 1)
 	get_parent().remove_child(self)
 
-
 func _on_QuitButton_button_down():
 	$QuitButton.material.set_shader_param("value", 0.1)
-
 
 func _on_QuitButton_mouse_entered():
 	$QuitButton.material.set_shader_param("value", 0.3)
 
-
 func _on_QuitButton_mouse_exited():
 	$QuitButton.material.set_shader_param("value", 1)
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#updates money and population as game continues
 	$ColorRect3/CityStatsBar/Money.text = "$" + Econ.comma_values(str(Econ.money))
 	$ColorRect3/CityStatsBar/Population.text = str(UpdatePopulation.get_population())
 	
 
+#When user adjusts tax sliders, functions are called to change the values in Econ.gd and update the text displayed to the user.
 func _on_PropertyTaxSlider_value_changed(value):
 	Econ.adjust_property_tax_rate(value)
 	$ColorRect3/CityStatsBar/HBoxContainer7/PropertyTaxValue.text = str(Econ.PROPERTY_TAX * 100) + "%"
-
-
 func _on_SalesTaxSlider_value_changed(value):
 	Econ.adjust_sales_tax_rate(value)
 	$ColorRect3/CityStatsBar/HBoxContainer8/SalesTaxValue.text = str(Econ.SALES_TAX * 100) + "%"
--- ui/Dashboard/VBoxContainer.gd
+++ ui/Dashboard/VBoxContainer.gd
@@ -1,5 +1,7 @@
 extends VBoxContainer
 
+var minus_icon = preload("res://assets/buttons/minus_button.png")
+var plus_icon = preload("res://assets/buttons/plus_button.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,23 +14,29 @@ func _ready():
 func _on_BadgesButton_pressed():
 	if $ColorRect.rect_min_size.y == 300:
 		$ColorRect.rect_min_size.y = 40
+		$ColorRect/BadgesButton.icon = plus_icon
 	else:
 		$ColorRect.rect_min_size.y = 300
+		$ColorRect/BadgesButton.icon = minus_icon
 	$ColorRect/UnlockedBadges.visible = !$ColorRect/UnlockedBadges.visible
 
 func _on_CharactersButton_pressed():
 	if $ColorRect2.rect_min_size.y == 300:
 		$ColorRect2.rect_min_size.y = 40
+		$ColorRect2/CharactersButton.icon = plus_icon
 	else:
 		$ColorRect2.rect_min_size.y = 300
+		$ColorRect2/CharactersButton.icon = minus_icon
 	$ColorRect2/DisplayCharacterCards.visible = !$ColorRect2/DisplayCharacterCards.visible
 
 func _on_SensorButton_pressed():
 	if $ColorRect3.rect_min_size.y == 300:
 		$ColorRect3.rect_min_size.y = 40
+		$ColorRect3/SensorButton.icon = plus_icon
 	else:
 		$ColorRect3.rect_min_size.y = 300
-
+		$ColorRect3/SensorButton.icon = minus_icon
+		
 func _on_ExpandAllButton_pressed():
 	$ColorRect.rect_min_size.y = 300
 	$ColorRect/UnlockedBadges.visible = true
