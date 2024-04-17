bedcd999e32e81f8d7c7d3f594ea5091c1d80d7a
Edward Menello <edmenello@gmail.com>
2024-04-17 12:49:15 -0400

    user name now saved when games saves

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -26,6 +26,8 @@ var mapHeight = 16
 var mapTool = Tool.NONE
 var tileMap = initTileMap()
 
+var userName = "noName"
+
 var seaLevel = 2       # World value that ocean resets to after a storm surge
 var oceanHeight = 0    # Current ocean level on map
 
@@ -68,7 +70,8 @@ func get_global_data():
 		"seaLevel": seaLevel,
 		"oceanHeight": oceanHeight,
 		"numZones": numZones,
-		"numPeople": numPeople
+		"numPeople": numPeople,
+		"userName": userName
 	}
 	
 	return globalData
