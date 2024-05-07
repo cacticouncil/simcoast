b3cfb7db861e9ef60721b34387565f71e8f7fe2c
Blake Thacker <62034641+acher0nparth@users.noreply.github.com>
2024-05-07 19:01:02 -0400

    deleting Tax Menu

--- AI/economy/presence_of_water.gd
+++ AI/economy/presence_of_water.gd
@@ -5,33 +5,9 @@ const INNER_NEIGHBOR = 0.10
 const OUTER_NEIGHBOR = 0.5
 
 
-# Is the zone near a body of water? If so, value increases
+# Currently this does nothing, but if I delete it then the desirability behavior tree stops working
+# so, it stays for now
 func _tick(agent: Node, blackboard: Blackboard) -> bool:
-#	var tile = blackboard.get_data("queue").front()
-#	var is_close_water = tile.is_close_water
-#	var is_far_water = tile.is_far_water
-#
-#	# Checking if this zone is touching a water tile
-#	if is_close_water == false:
-#		# Check all inner neighbor tiles to check for water
-#		var neighbors = [[tile.i-1, tile.j], [tile.i+1, tile.j], [tile.i, tile.j-1], [tile.i, tile.j+1]]
-#		for n in neighbors:
-#			if is_valid_tile(n[0], n[1]):
-#				if Global.tileMap[n[0]][n[1]].base == Tile.TileBase.OCEAN:
-#					is_close_water = true
-#
-#	# Checking if this zone is second neighbor to a water tile
-#	if is_far_water == false:		
-#		var far_neighbors = [[tile.i-2, tile.j], [tile.i+2, tile.j], [tile.i, tile.j-2], [tile.i, tile.j+2], \
-#			[tile.i+1, tile.j+1], [tile.i-1, tile.j+1], [tile.i-1, tile.j-1], [tile.i+1, tile.j-1]]
-#		for n in far_neighbors:
-#			if is_valid_tile(n[0], n[1]):
-#				if Global.tileMap[n[0]][n[1]].base == Tile.TileBase.OCEAN:
-#					is_far_water = true
-#
-#	# update tile variable booleans			
-#	tile.is_close_water = is_close_water
-#	tile.is_far_water = is_far_water
 						
 	return succeed()
 
